f233cbff891a9207b23936bcf9a85e9a63fa0991
slinghshot <simerpreet.singh40@gmail.com>
2024-11-20 12:41:25 -0800

    Updated Slider Icon

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-window.addEventListener('load', function() {
-    const slider = document.getElementById('slider');
-    const imageA = document.getElementById('imageA');
-    const imageB = document.getElementById('imageB');
+window.addEventListener('load', function () {
+  const slider = document.getElementById('slider');
+  const imageA = document.getElementById('imageA');
+  const imageB = document.getElementById('imageB');
+  const background = document.getElementById('background');
 
-    function updateSliderWidth() {
-        slider.style.width = `${imageA.clientWidth}px`;
-    }
+  function updateSliderWidth() {
+    // slider.style.width = `${background.clientWidth}px`;
+    slider.style.width = '1000px';
+  }
 
-    // Update slider width on load
-    updateSliderWidth();
+  // Update slider width on load
+  updateSliderWidth();
 
-    // Update slider width on window resize
-    window.addEventListener('resize', updateSliderWidth);
+  // Update slider width on window resize
+  window.addEventListener('resize', updateSliderWidth);
 
-    slider.addEventListener('input', (event) => {
-        const value = event.target.value;
-        imageA.style.opacity = 1 - value / 100;
-        imageB.style.opacity = value / 100;
-    });
+  slider.addEventListener('input', (event) => {
+    const value = event.target.value;
+    imageA.style.opacity = 1 - value / 100;
+    imageB.style.opacity = value / 100;
+  });
+
+  slider.addEventListener('mouseover', () => {
+    slider.style.setProperty('--thumb-image', 'url(SliderMick.png)');
+  });
+
+  slider.addEventListener('mouseout', () => {
+    slider.style.setProperty('--thumb-image', '#4CAF50');
+  });
 });
