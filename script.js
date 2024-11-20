7e9c5610aeb24a74e8fad29fe4a7ca6f1a98c3ca
slinghshot <simerpreet.singh40@gmail.com>
2024-11-20 14:17:03 -0800

    Added Audio

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ window.addEventListener('load', function () {
   const imageA = document.getElementById('imageA');
   const imageB = document.getElementById('imageB');
   const background = document.getElementById('background');
+  let audio = new Audio('MickOrBobby.ogg');
+  let audioImagine = new Audio('imagein.ogg');
+  let enableSecond = false;
 
   function updateSliderWidth() {
     // slider.style.width = `${background.clientWidth}px`;
@@ -19,6 +22,15 @@ window.addEventListener('load', function () {
     const value = event.target.value;
     imageA.style.opacity = 1 - value / 100;
     imageB.style.opacity = value / 100;
+    if (value == 100) {
+      audio.volume = 0.5;
+      audio.play();
+      enableSecond = true;
+    }
+    if (enableSecond && value == 0) {
+      audioImagine.volume = 0.8;
+      audioImagine.play();
+    }
   });
 
   slider.addEventListener('mouseover', () => {
